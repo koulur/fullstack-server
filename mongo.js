b1d2b2aec0f22881eb39e1fb07a46aa8cabe22bf
Rami <ramilejmi@gmail.com>
2019-09-02 11:23:30 +0300

    3.12

---
+++ mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3){
+    console.log('Give password as an argument.')
+    process.exit(1)
+}
+const password = process.argv[2]
+const url = `mongodb+srv://fullstack:${password}@cluster0-simwd.mongodb.net/persons?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true})
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(process.argv.length==3) {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(person)
+        })
+        mongoose.connection.close()
+    })
+}
+
+if(process.argv.length==5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+
+    person.save().then(response => {
+        console.log('Successfully saved: \n', response)
+        mongoose.connection.close()
+    })
+}
